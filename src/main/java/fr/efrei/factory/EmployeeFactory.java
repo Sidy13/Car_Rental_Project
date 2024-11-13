cedfe838e7bf83561fbbe1a846cb2d25c4107f67
Sidy <sidydcr.pro@gmail.com>
2024-11-13 14:14:10 +0200

    EmployeeFactory creation

---
+++ src/main/java/fr/efrei/factory/EmployeeFactory.java
@@ -0,0 +1,37 @@
+package fr.efrei.factory;
+
+import fr.efrei.domain.Address;
+import fr.efrei.domain.Employee;
+import fr.efrei.util.Helper;
+
+public class EmployeeFactory {
+    public static Employee createEmployee(String firstName, String lastName) {
+        if(Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName)) {
+            return null;
+        }
+        return (Employee) new Employee.Builder().setFirstName(firstName).setLastName(lastName).build();
+    }
+    public static Employee createEmployee(String firstName, String lastName, String email, String phone, Address address, int age, int employeeId, double salary, String password) {
+        if(Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(email) || Helper.isNullOrEmpty(phone) || Helper.isNullOrEmpty(password)) {
+            return null;
+        }
+        if(Helper.isNullOrEmpty(address)) {
+            return null;
+        }
+        if (age<=18 || employeeId<=0 || salary<=0) {
+            return null;
+        }
+        return  (Employee) new Employee.Builder()
+                .setEmployeeId(employeeId)
+                .setSalary(salary)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setPhone(phone)
+                .setAddress(address)
+                .setAge(age)
+                .setPassword(password)
+                .build();
+
+    }
+}
