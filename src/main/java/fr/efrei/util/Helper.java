93b584197b52d3c6f48e46ad80e800f8d02bba9d
Sidy <sidydcr.pro@gmail.com>
2024-11-14 17:19:26 +0200

    Random ID

--- src/main/java/fr/efrei/util/Helper.java
+++ src/main/java/fr/efrei/util/Helper.java
@@ -2,11 +2,16 @@ package fr.efrei.util;
 
 import fr.efrei.domain.Address;
 
+import java.util.UUID;
+
 public class Helper {
     public static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty();
     }
     public static boolean isNullOrEmpty(Address address) {
         return address == null;
+    }
+    public static String generateId(){
+        return UUID.randomUUID().toString();
     }
 }
