7f4bb7ec9f2c9437783a6fbc69448db2046c1c31
Sidy <sidydcr.pro@gmail.com>
2024-11-17 22:09:57 +0200

    isValidCard

---
+++ src/main/java/fr/efrei/domain/BankingCard.java
@@ -0,0 +1,36 @@
+package fr.efrei.domain;
+
+public class BankingCard {
+    private String number;
+    private String expirationDate;
+    private String cvv;
+    public BankingCard(String number, String expirationDate, String cvv) {
+        this.number = number;
+        this.expirationDate = expirationDate;
+        this.cvv = cvv;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+}
--- src/main/java/fr/efrei/util/Helper.java
+++ src/main/java/fr/efrei/util/Helper.java
@@ -1,7 +1,9 @@
 package fr.efrei.util;
 
 import fr.efrei.domain.Address;
+import fr.efrei.domain.BankingCard;
 
+import javax.smartcardio.Card;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
@@ -27,5 +29,13 @@ public class Helper {
     public static boolean isValidPassword(String password) {
         String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9!@#$%^&*()_+={}|;:,.<>?/-]).{8,}$";
         return password != null && Pattern.matches(passwordRegex, password);
+    }
+    public static boolean isValidCard(BankingCard card) {
+        // https://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
+        String mastercardRegex = "^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$";
+        String visaRegex = "^4[0-9]{12}(?:[0-9]{3})?$";
+        String visaMasterCardRegex = "^^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$";
+        String cardNumber = card.getNumber();
+        return Pattern.matches(mastercardRegex, cardNumber) || Pattern.matches(visaRegex, cardNumber) || Pattern.matches(visaMasterCardRegex, cardNumber);
     }
 }
