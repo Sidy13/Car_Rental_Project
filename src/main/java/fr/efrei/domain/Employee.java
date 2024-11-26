68bb32c7b364ddf0857f0e317fc6a0b9806f8e18
Sidy <sidydcr.pro@gmail.com>
2024-11-26 23:24:54 +0200

    Login and adminCreation methods

--- src/main/java/fr/efrei/domain/Employee.java
+++ src/main/java/fr/efrei/domain/Employee.java
@@ -1,5 +1,11 @@
 package fr.efrei.domain;
 
+import fr.efrei.factory.EmployeeFactory;
+import fr.efrei.repository.EmployeeRepository;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class Employee extends Person {
     private String employeeId;
     private double salary;
@@ -28,6 +34,40 @@ public class Employee extends Person {
     public double getSalary() {
         return salary;
     }
+
+    public Employee login(String email, String password, EmployeeRepository employeeRepository) {
+        List<Employee> employees = employeeRepository.getAll();
+        for (Employee employee : employees) {
+            if (employee.getEmail().equals(email) && employee.getPassword().equals(password)) {
+                System.out.println("Login successful for: " + email);
+                return employee;
+            }
+        }
+        System.out.println("Incorrect email or password");
+        return null;
+    }
+
+
+    public void adminCreation(EmployeeRepository employeeRepository) {
+        String defaultEmail = "admin@rental.com";
+        String defaultPassword = "Admin123@";
+
+        Address defaultAddress = new Address.Builder()
+                .setNumber(1)
+                .setStreet("Main Street")
+                .setCity("City")
+                .setState("State")
+                .setZip(12345)
+                .setCountry("Country")
+                .build();
+
+        Employee admin = EmployeeFactory.createEmployee("Admin", "User", defaultEmail, "0000000000", defaultAddress, 30, "0", 1, defaultPassword);
+        Employee createdAdmin = employeeRepository.create(admin);
+
+    }
+
+
+
 
 
 
